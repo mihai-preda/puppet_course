467ab0d4d0aa73df547eb7e4239d144c85c17a51
Mihai Preda <mpreda@outlook.com>
2024-12-23 17:08:04 -0800

    added manifests for zabbixdb and zabbixserver

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,3 +3,5 @@ node 'db.preda.ca' { include role::db }
 node default { include role::default }
 node 'puppet.preda.ca' { include role::puppetserver }
 node 'web.preda.ca' { include role::webserver }
+node 'monitor.preda.ca' { include role::zabbixserver }
+node 'zdb.preda' { include role::zabbixdb }
