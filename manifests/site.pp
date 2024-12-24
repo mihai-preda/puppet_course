cbe6ae74f98ca649a073d724a5a1ff0e751bb770
Mihai Preda <mpreda@outlook.com>
2024-12-23 17:12:56 -0800

    fixed typon in site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,4 +4,4 @@ node default { include role::default }
 node 'puppet.preda.ca' { include role::puppetserver }
 node 'web.preda.ca' { include role::webserver }
 node 'monitor.preda.ca' { include role::zabbixserver }
-node 'zdb.preda' { include role::zabbixdb }
+node 'zdb.preda.ca' { include role::zabbixdb }
