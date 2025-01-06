68b379e5ff3ec2c53eba7d4325a1a673cdd1ec9a
Mihai Preda <mpreda@outlook.com>
2025-01-05 18:17:03 -0800

    added puppet agent role to puppetserver

--- modules/role/manifests/puppetserver.pp
+++ modules/role/manifests/puppetserver.pp
@@ -1,6 +1,7 @@
 # puppt server class
 class role::puppetserver {
   include profile::base
+  include profile::puppet_agent
   include profile::git
   include profile::puppetserver
   include profile::r10k
