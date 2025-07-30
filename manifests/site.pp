49751c62423511e3cda36ed37a742316a447b8f0
Mihai Preda <mpreda@outlook.com>
2025-07-29 19:47:24 -0700

    renamed monitor host to monit

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,6 +3,6 @@ node 'db.preda.ca' { include role::db }
 node default { include role::default }
 node 'puppet.preda.ca' { include role::puppetserver }
 node 'web.preda.ca' { include role::webserver }
-node 'monitor.preda.ca' { include role::zabbixserver }
+node 'monit.preda.ca' { include role::zabbixserver }
 node 'zdb.preda.ca' { include role::zabbixdb }
 node 'app-srv1.preda.ca' { include role::app }
