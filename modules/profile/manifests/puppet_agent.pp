07a1d56705743c3bdad838c28384afc57827db6f
Mihai Preda <mpreda@outlook.com>
2025-09-20 15:22:07 -0700

    added collection to puppet_agent

--- modules/profile/manifests/puppet_agent.pp
+++ modules/profile/manifests/puppet_agent.pp
@@ -1,8 +1,9 @@
 # class puppet agent
 class profile::puppet_agent {
   class { 'puppet_agent':
+    colection       => 'puppet8',
     package_version => $facts['puppetversion'],
+    install_options => ['ENABLE_LONG_PATHS=true', 'PUPPET_SERVER=puppet.preda.ca', 'REINSTALLMODE="amus"'],
     config          => [{ section => main, setting => runinterval, value => '1h' }],
-    install_options => ['ENABLE_LONG_PATHS=true', 'PUPPET_SERVER=puppet.preda.ca'],
   }
 }
