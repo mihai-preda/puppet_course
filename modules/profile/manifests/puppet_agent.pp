158b340e88b5f4710b7dc939f32295a0e8f30579
Mihai Preda <mpreda@outlook.com>
2025-09-20 09:09:38 -0700

    updated puppet_agent ver

--- modules/profile/manifests/puppet_agent.pp
+++ modules/profile/manifests/puppet_agent.pp
@@ -1,8 +1,7 @@
 # class puppet agent
 class profile::puppet_agent {
   class { 'puppet_agent':
-    package_version => '8.10.0',
-    collection      => 'puppet8',
+    package_version => $facts['puppetversion'],
     config          => [{ section => main, setting => runinterval, value => '1h' }],
     install_options => ['ENABLE_LONG_PATHS=true', 'PUPPET_SERVER=puppet.preda.ca'],
   }
