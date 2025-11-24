822737778aff576fa0bbc37e6d5b54b07b1b4054
mihai-preda <35678814+mihai-preda@users.noreply.github.com>
2025-11-23 18:42:01 -0800

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -6,4 +6,3 @@ node 'web.preda.ca' { include role::webserver }
 node 'monit.preda.ca' { include role::zabbixserver }
 node 'zdb.preda.ca' { include role::zabbixdb }
 node 'app-srv1.preda.ca' { include role::app }
-node 'win-38njq8tg.preda.ca' { include role::windows }
