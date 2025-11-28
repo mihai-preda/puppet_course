cb703fa70c28a6b2b553d82a7a77bbd606d4d3e5
mihai-preda <35678814+mihai-preda@users.noreply.github.com>
2025-11-27 22:57:39 -0800

    upgraded hiera to v 7.0

--- modules/profile/manifests/puppetdb.pp
+++ modules/profile/manifests/puppetdb.pp
@@ -1,5 +1,18 @@
-# Class: puppetdb
+# profile puppetdb
 class profile::puppetdb {
   # Configure puppetdb and its underlying database
-  class { 'puppetdb': }
+  class { 'puppetdb':
+    postgresql_ssl_on       => true,
+    database_host           => 'db.preda.ca',
+    database_listen_address => '0.0.0.0',
+    database_password       => 'ctm3muf7tze!PYN@pvj',
+    node_ttl                => '0s',
+    node_purge_ttl          => '0s',
+  }
+  exec { '/opt/puppetlabs/bin/puppetdb ssl-setup -f': }
+  firewall { '100 allow http and https access':
+    dport => [8081, 8080],
+    proto => 'tcp',
+    jump  => 'accept',
+  }
 }
