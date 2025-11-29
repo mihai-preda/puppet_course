f468857a0dff59a9b5577c577c013b7136f5212a
mihai-preda <35678814+mihai-preda@users.noreply.github.com>
2025-11-28 20:59:22 -0800

    removed resolver

--- modules/profile/manifests/resolver.pp
+++
@@ -1,9 +0,0 @@
-# resolver profile
-class profile::resolver {
-  class { 'resolver':
-    method    => 'sysconfig',
-    servers   => ['10.21.2.254'],
-    domains   => ['preda.ca'],
-    interface => 'eth1',
-  }
-}
