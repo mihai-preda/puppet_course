8ad62063f8d5b8c67df1965fd36a5cb5b56b35b3
Mihai Preda <mpreda@outlook.com>
2025-12-28 19:27:10 -0800

    commented out the windows related pp agent conf

--- modules/profile/manifests/puppet_agent.pp
+++ modules/profile/manifests/puppet_agent.pp
@@ -3,7 +3,8 @@ class profile::puppet_agent {
   class { 'puppet_agent':
     collection      => 'puppet8',
     package_version => '8.10.0',
-    install_options => ['ENABLE_LONG_PATHS=true', 'PUPPET_SERVER=puppet.preda.ca', 'REINSTALLMODE="amus"'],
+    #the settings below are only valid on Windows
+    #install_options => ['ENABLE_LONG_PATHS=true', 'PUPPET_SERVER=puppet.preda.ca', 'REINSTALLMODE="amus"'],
     config          => [{ section => main, setting => runinterval, value => '1h' }],
   }
 }
