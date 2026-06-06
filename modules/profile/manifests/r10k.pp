b28491868fbc563e75f9edb1cbf3ae04c94988ba
Mihai Preda <mpreda@outlook.com>
2026-06-05 18:14:07 -0700

    added claude to ignore list

--- modules/profile/manifests/r10k.pp
+++ modules/profile/manifests/r10k.pp
@@ -1,7 +1,13 @@
 # Class r10k
 class profile::r10k {
   class { 'r10k':
-    remote => 'https://github.com/mihai-preda/control-repo',
+    sources => {
+      'control-repo' => {
+        'remote'                 => 'https://github.com/mihai-preda/control-repo',
+        'basedir'                => '/etc/puppetlabs/code/environments',
+        'ignore_branch_prefixes' => ['claude-'],
+      },
+    },
   }
   package { 'git':
     ensure => installed,
