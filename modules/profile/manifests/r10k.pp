e01a5c8ffe3501b6eaa1eef9004f60f7dac0b016
Mihai Preda <mpreda@outlook.com>
2026-06-05 18:35:06 -0700

    moved cachedir to top level

--- modules/profile/manifests/r10k.pp
+++ modules/profile/manifests/r10k.pp
@@ -1,12 +1,12 @@
 # Class r10k
 class profile::r10k {
   class { 'r10k':
-    sources => {
+    cachedir => '/var/cache/r10k',
+    sources  => {
       'control-repo' => {
         'remote'                 => 'https://github.com/mihai-preda/control-repo',
         'basedir'                => '/etc/puppetlabs/code/environments',
         'ignore_branch_prefixes' => ['claude-'],
-        cachedir                 => '/var/cache/r10k',
       },
     },
   }
