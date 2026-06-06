3a3e72fc99c55b522e5e009f55347a0cee3f7e72
Mihai Preda <mpreda@outlook.com>
2026-06-05 18:24:19 -0700

    added cachedir parameter to r10k configuration

--- modules/profile/manifests/r10k.pp
+++ modules/profile/manifests/r10k.pp
@@ -6,6 +6,7 @@ class profile::r10k {
         'remote'                 => 'https://github.com/mihai-preda/control-repo',
         'basedir'                => '/etc/puppetlabs/code/environments',
         'ignore_branch_prefixes' => ['claude-'],
+        cachedir                 => '/var/cache/r10k',
       },
     },
   }
